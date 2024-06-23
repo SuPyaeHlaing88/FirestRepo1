eb21299dd5c087fd04a3c9a2f451e92b3e5296fb
Thazin96 <thazinwaibt7@gmail.com>
2024-06-23 14:39:05 +0630

    add_sub method

--- src/main/java/examples/javaTest.java
+++ src/main/java/examples/javaTest.java
@@ -1,9 +1,13 @@
 package examples;
-
 public class javaTest {
 int a=10, b=5;
 public void add(int a, int b) {
 	int sum=a+b;
 	System.out.println("sum:" + sum);
 }
+public void sub(int a, int b) {
+	int sub=a-b;
+	System.out.println("sub:" + sub);
+}
+
 }
